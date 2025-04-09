4923cd553766feaf66fe817c73c49a4d3e025d9e
DownyDowner <boris.vancauter@gmail.com>
2025-04-09 12:41:08 +0200

    fix: swagger support

--- GameLogAPI/Program.cs
+++ GameLogAPI/Program.cs
@@ -4,20 +4,21 @@ using GameLogAPI.src.Data;
 using GameLogAPI.src.Features.Games;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
+var builder = WebApplication.CreateBuilder();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<GameDbContext>(options =>
     options.UseSqlite(connectionString));
 
-builder.Services.AddFastEndpoints()
-    .AddSwaggerDocument();
+builder.Services
+   .AddFastEndpoints()
+   .SwaggerDocument();
 
 builder.Services.AddScoped<GameService>();
 
 var app = builder.Build();
-app.UseFastEndpoints();
-app.UseSwaggerGen();
+app.UseFastEndpoints()
+   .UseSwaggerGen();
+app.Run();
 
 app.Run();
