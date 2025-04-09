422e6c5bcf727fbeff94abb26c57dc84efe895c1
DownyDowner <boris.vancauter@gmail.com>
2025-04-09 13:43:33 +0200

    feat: add endpoint to create a new game

---
+++ GameLogAPI/src/Features/Games/AddGame.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+
+namespace GameLogAPI.src.Features.Games {
+    public class AddGameEndpoint(GameService service) : Endpoint<AddGameRequest> {
+        public override void Configure() {
+            Post("games");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(AddGameRequest req, CancellationToken ct) {
+            var id = await service.AddGame(req, ct);
+            await SendOkAsync(id, ct);
+        }
+    }
+
+    public record AddGameRequest(string Title, string Platform);
+}
--- GameLogAPI/src/Features/Games/GameService.cs
+++ GameLogAPI/src/Features/Games/GameService.cs
@@ -4,6 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GameLogAPI.src.Features.Games {
     public class GameService(GameDbContext context) {
+        internal async Task<Guid> AddGame(AddGameRequest req, CancellationToken ct) {
+            var game = new Game { Title= req.Title, Platform = req.Platform };
+            await context.Games.AddAsync(game, ct);
+            await context.SaveChangesAsync();
+
+            return game.Id;
+        }
+
         internal async Task<IEnumerable<Game>> GetGames(CancellationToken ct) {
             return await context
                 .Games
